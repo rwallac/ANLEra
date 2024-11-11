4ca6a050806f3eea18199c51f9f76e8ae2311e55
Severin Glöckner <severin.gloeckner@stud.htwk-leipzig.de>
2024-11-11 04:46:47 +0100

    simplify unit_type check

--- utils/research.lua
+++ utils/research.lua
@@ -186,12 +186,7 @@ local human_units = {'Spearman', 'Fencer', 'Heavy Infantryman', 'Sergeant', 'Bow
 local orcish_units = {'Orcish Grunt', 'Orcish Archer', 'Orcish Assassin', 'Troll Whelp', 'Wolf Rider'}
 -- https://www.reddit.com/r/wesnoth/comments/15799mv/myrline_the_outcasts
 local outlaw_units = {'Thug', 'Thief', 'Footpad', 'Poacher', 'Wolf', 'Young Ogre'}
-local undead_units
-if wesnoth.unit_types['Skeleton Rider'] == nil then
-    undead_units = {'Skeleton', 'Skeleton Archer', 'Vampire Bat', 'Ghost', 'Ghoul'}
-else
-    undead_units = {'Skeleton', 'Skeleton Archer', 'Vampire Bat', 'Ghost', 'Ghoul', 'Skeleton Rider'}
-end
+local undead_units = {'Skeleton', 'Skeleton Archer', 'Vampire Bat', 'Ghost', 'Ghoul', wesnoth.unit_types['Skeleton Rider'] and 'Skeleton Rider'}
 local dunefolk_units = {'Dune Burner', 'Dune Soldier', 'Dune Skirmisher', 'Dune Rover', 'Dune Rider'}
 local special_units = {'Giant Mudcrawler', 'Great Icemonax'}
 
